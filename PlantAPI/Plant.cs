c0e0a233a8cd3188121a7bfdd40939a325f83bd6
Sylvain Aroma <sylvain.aroma@alten.nl>
2022-01-05 09:44:11 +0100

    first commit

---
+++ PlantAPI/Plant.cs
@@ -0,0 +1,9 @@
+﻿namespace PlantAPI
+{
+    public class Plant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string WaterInterval { get; set; }
+    }
+}
